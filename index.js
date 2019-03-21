4a1a7388cf72ed929482ae1e125cedc94507021e
Zachary Lehmann <zlehmann@gmail.com>
2019-03-21 19:55:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,22 @@ class Driver {
     return Math.abs(Math.round(exp/365.25))-3;
   }
 }
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled() {
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+  }
+}
