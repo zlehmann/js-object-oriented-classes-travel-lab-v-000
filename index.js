d9cfb2491676262930c095d431c289168408a8a3
Zachary Lehmann <zlehmann@gmail.com>
2019-03-21 19:22:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Driver {
     this.join_day = join_day
   }
 
-  startDate(join_day) {
+  startDate() {
     console.log(join_day);
     let start_date = new Date(join_day);
     return start_date;
