a03de37ca4bbe9845c0c7955bdf82d1bec5409b4
Zachary Lehmann <zlehmann@gmail.com>
2019-03-21 19:35:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ class Driver {
   constructor(name, join_day) {
     this.name = name
     this.join_day = join_day
-  }
-
-  startDate() {
-    let start_date = new Date(this.join_day);
-    console.log(typeof start_date);
-    return start_date;
+    this.startDate = new Date(this.join_day)
   }
 
   yearsExperienceFromBeginningOf (start_year) {
