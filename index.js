492d341a90de893749af848716defd59580984eb
Zachary Lehmann <zlehmann@gmail.com>
2019-03-21 19:13:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+class Driver {
+  constructor(name, join_day) {
+    this.name = name
+    this.join_day = join_day
+  }
+
+  startDate(join_day) {
+    return Date(join_day);
+  }
+
+  yearsExperienceFromBeginningOf (start_year) {
+
+  }
+}
